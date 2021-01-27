07821b18c3ec9b09c0eaa948d48024121377f4af
davidschraedel <65734436+davidschraedel@users.noreply.github.com>
2021-01-26 17:34:45 -0700

    Add project files.

---
+++ FirstASPNetApp/Models/GradeCalculatorModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace FirstASPNetApp.Models
+{
+    public class GradeCalculatorModel
+    {
+        private const double MIN = 0.0;
+
+        [Required]
+        [Range(minimum: MIN, maximum: 10.0)]
+        public double assignments { get; set; }
+        
+        [Range(minimum: MIN, maximum: 10.0)]
+        public double groupProject { get; set; }
+        
+        [Range(minimum: MIN, maximum: 10.0)]
+        public double quizzes { get; set; }
+        
+        [Range(minimum: MIN, maximum: 20.0)]
+        public double exams { get; set; }
+        
+        [Range(minimum: MIN, maximum: 10.0)]
+        public double intex { get; set; }
+    }
+}
---
+++ FirstASPNetApp/wwwroot/js/script.js
@@ -0,0 +1,59 @@
+﻿//David Schraedel
+//JavaScript for grade calculation
+
+
+$("#form").submit(function handleForm(event) { event.preventDefault(); })
+
+$('#submitButton').click(function () {
+    //define variables
+    var fGradePercentage = 0.0;
+    var sGradeLetter = '';
+    var fAssignments = parseFloat($('#assignments').val());
+    var fGroupProject = parseFloat($('#groupProject').val());
+    var fQuizzes = parseFloat($('#quizzes').val());
+    var fExams = parseFloat($('#exams').val());
+    var fIntex = parseFloat($('#intex').val());
+
+    //calculate grade based on inputs
+    fGradePercentage = fAssignments + fGroupProject + fQuizzes + fExams + fIntex;
+    if (fGradePercentage >= 94) {
+        sGradeLetter = 'A';
+    }
+    else if (fGradePercentage >= 90) {
+        sGradeLetter = 'A-';
+    }
+    else if (fGradePercentage >= 87) {
+        sGradeLetter = 'B+';
+    }
+    else if (fGradePercentage >= 84) {
+        sGradeLetter = 'B';
+    }
+    else if (fGradePercentage >= 80) {
+        sGradeLetter = 'B-';
+    }
+    else if (fGradePercentage >= 77) {
+        sGradeLetter = 'C+';
+    }
+    else if (fGradePercentage >= 74) {
+        sGradeLetter = 'C';
+    }
+    else if (fGradePercentage >= 70) {
+        sGradeLetter = 'C-';
+    }
+    else if (fGradePercentage >= 67) {
+        sGradeLetter = 'D+';
+    }
+    else if (fGradePercentage >= 64) {
+        sGradeLetter = 'D';
+    }
+    else if (fGradePercentage >= 60) {
+        sGradeLetter = 'D-';
+    }
+    else {
+        sGradeLetter = 'E';
+    }
+
+    //display calculated products on page
+    $('#calculatedLetterGrade').html(sGradeLetter);
+    $('#calculatedPercentGrade').html(fGradePercentage + "%");
+});
